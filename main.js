299c39707602416a9253adfaa092153e3a9bff2d
Santiago Beltran <s.beltran10@uniandes.edu.co>
2017-05-05 11:00:19 -0500

    Primer commit

--- main.js
+++ main.js
@@ -1,16 +1,41 @@
 var grafo = [];
 
-grafo[0] = [{verticeAdyacente:4,peso:4}];
-grafo[1] = [{verticeAdyacente:3,peso:1}];
-grafo[2] = [{verticeAdyacente:0,peso:2}, {verticeAdyacente:4,peso:4}];
-grafo[3] = [{verticeAdyacente:2,peso:3}];
-grafo[4] = [{verticeAdyacente:3,peso:8}, {verticeAdyacente:1,peso:8}];
-grafo[5] = [{verticeAdyacente:2,peso:6},{verticeAdyacente:3,peso:4}];
+// Se crea la lista de adyacencia que representa al grafo
+
+var vertice0 = [
+    { verticeAdyacente: 4, peso: 4 }
+];
+var vertice1 = [
+    { verticeAdyacente: 3, peso: 1 }
+];
+var vertice2 = [
+    { verticeAdyacente: 0, peso: 2 },
+    { verticeAdyacente: 4, peso: 4 }
+];
+var vertice3 = [
+    { verticeAdyacente: 2, peso: 3 }
+];
+var vertice4 = [
+    { verticeAdyacente: 3, peso: 8 },
+    { verticeAdyacente: 1, peso: 8 }
+];
+var vertice5 = [
+    { verticeAdyacente: 2, peso: 6 },
+    { verticeAdyacente: 3, peso: 4 }
+];
+
+grafo.push(vertice0);
+grafo.push(vertice1);
+grafo.push(vertice2);
+grafo.push(vertice3);
+grafo.push(vertice4);
+grafo.push(vertice5);
+
+console.log(grafo);
 
 // PARTE 1
 
 
 // PARTE 2
-
 var grafoAlternativo;
 
